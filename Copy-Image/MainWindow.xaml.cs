a2ce42ea448bbf04f3da7062058d69b9952d9dae
Jared Goodwin <jared@translucency.info>
2017-05-26 05:20:47 -0700

    Bug fixes.

--- Copy-Image/MainWindow.xaml.cs
+++ Copy-Image/MainWindow.xaml.cs
@@ -14,7 +14,6 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Copy_Image
 {
@@ -102,17 +101,14 @@ namespace Copy_Image
             if (targetPath != null)
             {
                 this.Hide();
-                Clipboard.SetImage(new BitmapImage(new Uri(targetPath)));
-                var cursor = System.Windows.Forms.Cursor.Position;
-                var tooltip = new ToolTip();
-                tooltip.Content = "Image copied to clipboard!";
-                tooltip.Placement = System.Windows.Controls.Primitives.PlacementMode.AbsolutePoint;
-                tooltip.HorizontalOffset = cursor.X - 5;
-                tooltip.VerticalOffset = cursor.Y;
-                tooltip.IsOpen = true;
-                await Task.Delay(2000);
-                tooltip.BeginAnimation(OpacityProperty, new DoubleAnimation(0, TimeSpan.FromSeconds(2)));
-                await Task.Delay(2500);
+                try
+                {
+                    Clipboard.SetImage(new BitmapImage(new Uri(targetPath.Trim())));
+                    await ShowTooltip("Image copied to clipboard!");
+                }
+                catch
+                {
+                }
                 this.Close();
             }
             else
@@ -130,6 +126,21 @@ namespace Copy_Image
             }
 
         }
+
+        private async Task ShowTooltip(string v)
+        {
+            var cursor = System.Windows.Forms.Cursor.Position;
+            var tooltip = new ToolTip();
+            tooltip.Content = "Image copied to clipboard!";
+            tooltip.Placement = System.Windows.Controls.Primitives.PlacementMode.AbsolutePoint;
+            tooltip.HorizontalOffset = cursor.X - 5;
+            tooltip.VerticalOffset = cursor.Y;
+            tooltip.IsOpen = true;
+            await Task.Delay(2000);
+            tooltip.BeginAnimation(OpacityProperty, new DoubleAnimation(0, TimeSpan.FromSeconds(2)));
+            await Task.Delay(2500);
+        }
+
         private void buttonInstall_Click(Object sender, RoutedEventArgs e)
         {
             var assem = System.Reflection.Assembly.GetExecutingAssembly();
@@ -138,12 +149,19 @@ namespace Copy_Image
             var content = reRead.ReadToEnd();
             reRead.Close();
             reStream.Close();
-            File.WriteAllText(System.IO.Path.GetTempPath() + @"\ImageReg.reg", content);
-            var psi = new ProcessStartInfo("cmd.exe", String.Format("/c reg.exe import {0}\\ImageReg.reg&mkdir \"{1}\"&copy \"{2}\" \"{3}\" /y", System.IO.Path.GetTempPath(), System.IO.Path.GetDirectoryName(installedPath), Application.ResourceAssembly.ManifestModule.Assembly.Location, installedPath));
+            File.WriteAllText(Path.GetTempPath() + @"\ImageReg.reg", content);
+            var psi = new ProcessStartInfo("cmd.exe", String.Format("/c reg.exe import {0}\\ImageReg.reg&mkdir \"{1}\"&copy \"{2}\" \"{3}\" /y", Path.GetTempPath(), Path.GetDirectoryName(installedPath), Application.ResourceAssembly.ManifestModule.Assembly.Location, installedPath));
             psi.WindowStyle = ProcessWindowStyle.Hidden;
             psi.Verb = "runas";
-            var proc = Process.Start(psi);
-            proc.WaitForExit();
+            try
+            {
+                var proc = Process.Start(psi);
+                proc.WaitForExit();
+            }
+            catch
+            {
+                return;
+            }
             buttonInstall.IsEnabled = false;
             buttonRemove.IsEnabled = true;
             MessageBox.Show("Install completed!  If the 'Copy Image' option isn't showing up, reset your program defaults and reinstall Copy-Image.", "Install Completed", MessageBoxButton.OK, MessageBoxImage.Information);
@@ -151,11 +169,18 @@ namespace Copy_Image
 
         private void buttonRemove_Click(Object sender, RoutedEventArgs e)
         {
-            var psi = new ProcessStartInfo("cmd.exe", "/c rd \"" + installedPath + @""" /s /q&reg.exe delete HKCR\pngfile\shell\CopyImage /f&reg.exe delete HKCR\jpegfile\shell\CopyImage /f");
+            var psi = new ProcessStartInfo("cmd.exe", "/c rd \"" + Path.GetDirectoryName(installedPath) + @""" /s /q&reg.exe delete HKCR\pngfile\shell\CopyImage /f&reg.exe delete HKCR\jpegfile\shell\CopyImage /f");
             psi.WindowStyle = ProcessWindowStyle.Hidden;
             psi.Verb = "runas";
-            var proc = Process.Start(psi);
-            proc.WaitForExit();
+            try
+            {
+                var proc = Process.Start(psi);
+                proc.WaitForExit();
+            }
+            catch
+            {
+                return;
+            }
             MessageBox.Show("Copy-Image has been removed.", "Uninstall Completed", MessageBoxButton.OK, MessageBoxImage.Information);
             buttonInstall.IsEnabled = true;
             buttonRemove.IsEnabled = false;
